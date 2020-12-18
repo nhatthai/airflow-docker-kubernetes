40706d3ca980f8eafc7dd2132ef05c2abe175af0
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2020-12-18 08:36:31 +0700

    Using Kubernetes Pod Operator

---
+++ airflow-k8s-pod-operator/dags/example-pod-operator.py
@@ -0,0 +1,55 @@
+import logging
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+
+log = logging.getLogger(__name__)
+
+dag = DAG(
+    "example_using_k8s_pod_operator",
+    schedule_interval="0 1 * * *",
+    catchup=False,
+    default_args={
+        "owner": "admin",
+        "depends_on_past": False,
+        "start_date": datetime(2020, 8, 7),
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 2,
+        "retry_delay": timedelta(seconds=30),
+        "sla": timedelta(hours=23),
+    },
+)
+
+with dag:
+    task_1 = KubernetesPodOperator(
+        image="ubuntu:16.04",
+        namespace="default",
+        cmds=["bash", "-cx"],
+        arguments=["pwd"],
+        labels={"foo": "bar"},
+        name="test-using-k8spodoperator-task-1",
+        task_id="task-1-echo",
+        is_delete_operator_pod=False,
+        image_pull_policy='Always',
+        in_cluster=True,
+    )
+    task_2 = KubernetesPodOperator(
+        image="ubuntu:16.04",
+        namespace="default",
+        cmds=["sleep"],
+        arguments=["300"],
+        labels={"foo": "bar"},
+        name="test-using-k8spodoperator-task-2",
+        task_id="task-2-sleep",
+        is_delete_operator_pod=False,
+        image_pull_policy='Always',
+        in_cluster=True,
+    )
+
+task_1
+task_2
